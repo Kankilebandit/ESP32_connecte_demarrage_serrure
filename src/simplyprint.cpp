373389869f94c4819532857affca0a6ddc0cc38c
CeJoseph <120047802+CeJoseph@users.noreply.github.com>
2024-11-20 12:47:23 -0500

    Update Continue Function

--- src/simplyprint.cpp
+++ src/simplyprint.cpp
@@ -46,14 +46,11 @@ String GetState(int printer_id)
 
 int continuer_impression(int printer_id)
 {
-    int Continuer = 0;
-
     HTTPClient http;
-    String path = "printers/Get";
-    http.begin((API_HOST + path));
+    String path = "printers/actions/Pause?pid=";
+    http.begin((API_HOST + path + printer_id));
     http.addHeader("accept", "application/json");
     http.addHeader("X-API-KEY", API_KEY);
-
     int httpResponseCode = http.POST("{}");
 
     if (httpResponseCode > 0)
@@ -66,63 +63,17 @@ int continuer_impression(int printer_id)
 
         if (doc["status"] == true)
         {
-            JsonArray data = doc["data"];
+            return 1; // pause successful
+        }
+        else
+        {
+            /*JsonArray data = doc["data"];
 
             for (JsonObject printerObj : data)
             {
-                const char *printerName = printerObj["printer"]["name"];
-                const char *printerState = printerObj["printer"]["state"];
-
-                int printerId = printerObj["id"];
-
-                if (printerId == printer_id)
-                {
-                    if (strcmp(printerState, "printing") == 0)
-                    {
-                        Continuer = 1;
-                    }
-                    else
-                    {
-                        return 0; // cannot pause
-                    }
-                }
-            }
-        }
-        http.end();
-    }
-
-    if (Continuer == 1)
-    {
-
-        String path = "printers/actions/Pause?pid=";
-        http.begin((API_HOST + path + printer_id));
-        http.addHeader("accept", "application/json");
-        http.addHeader("X-API-KEY", API_KEY);
-
-        httpResponseCode = http.POST("{}");
-
-        if (httpResponseCode > 0)
-        {
-            String response = http.getString();
-
-            const size_t capacity = 10 * JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(2) + 1024;
-            DynamicJsonDocument doc(capacity);
-            deserializeJson(doc, response);
-
-            if (doc["status"] == true)
-            {
-                return 1; // pause successful
-            }
-            else
-            {
-                /*JsonArray data = doc["data"];
-
-                for (JsonObject printerObj : data)
-                {
-                    printerObj[""]
-                }*/
-                return 0; // cannot pause
-            }
+                printerObj[""]
+            }*/
+            return 0; // cannot pause
         }
     }
 }
